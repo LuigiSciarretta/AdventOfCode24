ec01a7e92e51b0595a436ab68fab418ca1b0839e
LuigiSciarretta <82830074+LuigiSciarretta@users.noreply.github.com>
2024-12-02 15:24:37 +0100

    Day2- second solution

--- Day2/day2.py
+++ Day2/day2.py
@@ -13,11 +13,20 @@ def read_input(file_name:str) -> str:
     return content
 
 
+
 def manage_input(content: str) -> list[list[int]]:
     rows = content.strip().split('\n')  
     final_report = [[int(value) for value in row.split(' ')] for row in rows]
     return final_report
 
+
+
+def check_distance(distance: int) -> bool:
+    abs_distance = abs(distance)
+    if abs_distance >= 1 and abs_distance <=3: 
+        return True
+    else:
+        return False
 
 
 
@@ -58,14 +67,55 @@ def count_safe_report(final_report: list[list[int]]):
             count_safe_repo+=1
 
     return count_safe_repo
-            
 
-def check_distance(distance: int) -> bool:
-    abs_distance = abs(distance)
-    if abs_distance >= 1 and abs_distance <=3: 
-        return True
-    else:
-        return False
+
+
+def count_new_safe_report(final_report: list[list[int]]):
+    count_safe_repo = 0
+
+    for report in final_report:
+        valid_report = False
+        for i in range(len(report)):
+            valid_sub_report = True
+            residual_list = report[:i] + report[i+1:]
+            increasing = False
+            if len(residual_list) > 1:
+                first_distance = residual_list[1] - residual_list[0]
+                if first_distance > 0:
+                    increasing = True
+                else:
+                    increasing = False 
+        
+            for i in range(len(residual_list) -1):
+                distance = residual_list[i+1] - residual_list[i] 
+                # increasing case
+                if increasing is True:
+                    if distance < 0:
+                        valid_sub_report = False
+                        break
+                    else:
+                        admissible_distance = check_distance(distance)
+                        if admissible_distance is False:
+                            valid_sub_report = False
+
+                else: #decreasing case
+                    if distance > 0:
+                        valid_sub_report = False
+                        break
+                    else:
+                        admissible_distance = check_distance(distance)
+                        if admissible_distance is False:
+                            valid_sub_report = False
+                            break
+
+            if valid_sub_report == True:
+                valid_report = True
+                break
+        
+        if valid_report == True:
+            count_safe_repo+=1
+
+    return count_safe_repo
 
 
 
@@ -73,7 +123,9 @@ def main():
     input = read_input('input')
     final_report = manage_input(input)
     count_safe_repo = count_safe_report(final_report)
-    print(f"Number of safe report: {count_safe_repo}")
+    count_new_safe_repo = count_new_safe_report(final_report)
+    print(f"Number of safe report (first solution): {count_safe_repo}")
+    print(f"Number of new safe report (second solution): {count_new_safe_repo}")
 
 
 
