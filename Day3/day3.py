6758fd42aa1eb36aa49a6263b44dd24cd085933e
LuigiSciarretta <82830074+LuigiSciarretta@users.noreply.github.com>
2024-12-03 17:01:30 +0100

    Day3 - second solution

--- Day3/day3.py
+++ Day3/day3.py
@@ -14,8 +14,7 @@ def read_input(file_name:str) -> str:
     return content
 
 
-def capture_valid_istruction(input: str) -> int:
-    import re
+def capture_valid_instruction(input: str) -> int:
     multiply_pattern = re.compile(r'mul\((\d+),(\d+)\)')
     all_patterns = multiply_pattern.findall(input)
 
@@ -26,12 +25,42 @@ def capture_valid_istruction(input: str) -> int:
         print("Problem with regex")
 
 
+def capture_valid_instruction_with_statment(input: str) -> int:
+    do_pattern = re.compile(r'do\(\)')
+    dont_pattern = re.compile(r"don't\(\)")
+
+    do_match = [match.start() for match in re.finditer(do_pattern, input)]
+    dont_match = [match.start() for match in re.finditer(dont_pattern, input)]
+
+    enable_multiply = True  
+    total_sum = 0
+    last_position = 0  
+
+    do_list = [(start_pos, 'do') for start_pos in do_match]
+    dont_list = [(start_pos, 'dont') for start_pos in dont_match]
+    do_dont_list = sorted(do_list + dont_list)
+    
+    for start_pos, pattern_type in do_dont_list:
+        segment = input[last_position:start_pos]
+        if enable_multiply:
+            total_sum += capture_valid_instruction(segment)
+        last_position = start_pos
+        if pattern_type == 'do':
+            enable_multiply = True
+        elif pattern_type == 'dont':
+            enable_multiply = False
+    
+    final_segment = input[last_position:]
+    if enable_multiply:
+        total_sum += capture_valid_instruction(final_segment)
+
+    return total_sum
+    
 
 def main():  
     input = read_input('input')
-    solution1 = capture_valid_istruction(input)
-    print(f"Ths solution of first puzzle is {solution1}")
-
+    print(f"Ths solution of first puzzle is {capture_valid_instruction(input)}")
+    print(f"Ths solution of second puzzle is {capture_valid_instruction_with_statment(input)}")
 
 if __name__ == '__main__':
     main()
